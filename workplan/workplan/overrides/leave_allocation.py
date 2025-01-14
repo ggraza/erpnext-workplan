4f28d21379447adc7e3a5a81fde2105925b5bf20
Moritz Stückler <moritz@bitbetter.de>
2025-01-14 13:10:11 +0100

    fix: bug in leave allocation

--- workplan/workplan/overrides/leave_allocation.py
+++ workplan/workplan/overrides/leave_allocation.py
@@ -1,11 +1,12 @@
 import frappe
 
 def update(doc, event):
-	leave_policy_assignment = frappe.get_doc("Leave Policy Assignment", doc.leave_policy_assignment)
-	
-	if leave_policy_assignment.custom_use_workplan_for_allocation_calculation:
-		employee = frappe.get_doc("Employee", doc.employee)
-		weekly_hours = employee.custom_monday + employee.custom_tuesday + employee.custom_wednesday + employee.custom_thursday + employee.custom_friday
-		doc.new_leaves_allocated = doc.new_leaves_allocated * (weekly_hours/40)
+	if doc.leave_policy_assignment:
+		leave_policy_assignment = frappe.get_doc("Leave Policy Assignment", doc.leave_policy_assignment)
+
+		if leave_policy_assignment.custom_use_workplan_for_allocation_calculation:
+			employee = frappe.get_doc("Employee", doc.employee)
+			weekly_hours = employee.custom_monday + employee.custom_tuesday + employee.custom_wednesday + employee.custom_thursday + employee.custom_friday
+			doc.new_leaves_allocated = doc.new_leaves_allocated * (weekly_hours/40)
 
 	return doc
