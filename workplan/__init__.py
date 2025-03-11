ebe6c8cd93639fdeb0dfa8807adc2a380b19e9a8
Fabian Schmidt <fabian@bitbetter.de>
2025-03-11 19:29:30 +0100

    path

--- workplan/__init__.py
+++ workplan/__init__.py
@@ -4,4 +4,4 @@ import hrms.hr.doctype.leave_application.leave_application
 import workplan.workplan.overrides.leave_application
 
 # Monkey patching this method into the existing DocType, because the method is not a class method which can be extended/overridden
-hrms.hr.doctype.leave_application.leave_application.get_number_of_leave_days = get_number_of_leave_days
+hrms.hr.doctype.leave_application.leave_application.get_number_of_leave_days = workplan.workplan.overrides.leave_application.get_number_of_leave_days
