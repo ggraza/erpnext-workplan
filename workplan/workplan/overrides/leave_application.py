e5a8b2e9c6de96613c747f2f2da6afe7725d0c4f
Fabian Schmidt <fabian@bitbetter.de>
2025-03-11 11:48:13 +0100

    logging

--- workplan/workplan/overrides/leave_application.py
+++ workplan/workplan/overrides/leave_application.py
@@ -10,6 +10,9 @@ from hrms.hr.doctype.leave_application.leave_application import (get_holidays)
 from hrms.utils.holiday_list import (get_holiday_dates_between)
 from erpnext.setup.doctype.employee.employee import get_holiday_list_for_employee
 
+frappe.utils.logger.set_log_level("DEBUG")
+logger = frappe.logger("workplan", allow_site=True, file_count=50)
+
 
 @frappe.whitelist()
 def get_number_of_leave_days(
@@ -23,48 +26,57 @@ def get_number_of_leave_days(
 ) -> float:
 	"""Returns number of leave days between 2 dates after considering half day and holidays
 	(Based on the include_holiday setting in Leave Type)"""
-	
+
+	logger.debug(f"get_number_of_leave_days {employee} {leave_type} {from_date} {to_date}")
 	number_of_weekdays = get_weekdays_diff(from_date, to_date)
-	
-	print(number_of_weekdays)
+
+	logger.debug(f"number_of_weekdays {number_of_weekdays}")
 
 	if not frappe.db.get_value("Leave Type", leave_type, "include_holiday"):
+		logger.debug(f"Leave Type {leave_type} not includes holidays as leaves")
 		holiday_list_local = get_holiday_list_for_employee(employee)
-		holidays_between_from_to : list[datetime.date] = get_holiday_dates_between(holiday_list_local, from_date.isoformat(), to_date.isoformat())
+		holidays_between_from_to: list[datetime.date] = get_holiday_dates_between(
+			holiday_list_local, from_date.isoformat(), to_date.isoformat())
 		for holiday in holidays_between_from_to:
-			number_of_weekdays[holiday.weekday()] = number_of_weekdays[holiday.weekday()] -1 if number_of_weekdays[holiday.weekday()] > 0 else 0
+			number_of_weekdays[holiday.weekday()] = number_of_weekdays[holiday.weekday()] - 1 if \
+				number_of_weekdays[holiday.weekday()] > 0 else 0
+	else:
+		logger.debug(f"Leave Type {leave_type} includes holidays as leaves")
 
 	employee = frappe.get_doc("Employee", employee)
 	sum_working_hours = 0
 	for i, days in enumerate(number_of_weekdays):
 		match i:
-			case 0: 
+			case 0:
 				sum_working_hours += employee.custom_monday * days
-			case 1: 
+			case 1:
 				sum_working_hours += employee.custom_tuesday * days
-			case 2: 
+			case 2:
 				sum_working_hours += employee.custom_wednesday * days
-			case 3: 
+			case 3:
 				sum_working_hours += employee.custom_thursday * days
-			case 4: 
+			case 4:
 				sum_working_hours += employee.custom_friday * days
 
+	logger.debug(f"Sum_working_hours {sum_working_hours}")
 	return sum_working_hours / 8
+
 
 def get_weekdays_diff(from_date: datetime.date, to_date: datetime.date):
 	firstWeekday = from_date.weekday()
 	numberOfDays = date_diff(to_date, from_date) + 1
-	fullWeeks = int(numberOfDays / 7);
+	fullWeeks = int(numberOfDays / 7)
 	additionalDays = numberOfDays - (fullWeeks * 7)
 
 	result = [
-		 	fullWeeks, fullWeeks,fullWeeks,fullWeeks,fullWeeks,fullWeeks,fullWeeks
+		fullWeeks, fullWeeks, fullWeeks, fullWeeks, fullWeeks, fullWeeks, fullWeeks
 	]
 
 	for x in range(additionalDays):
 		result[(firstWeekday + x) % 7] += 1
 
-	return result	
+	return result
+
 
 # Monkey patching this method into the existing DocType, because the method is not a class method which can be extended/overridden
 hrms.hr.doctype.leave_application.leave_application.get_number_of_leave_days = get_number_of_leave_days
